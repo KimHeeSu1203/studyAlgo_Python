0cac6899e62232bd1a61adecdafc6f29fdb86066
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-04-05 23:31:16 +0900

    220405 / lv.1 / 하샤드 수 - 다른사람의 풀이

--- 12947.py
+++ 12947.py
@@ -2,5 +2,7 @@ def solution(x):
     print(sum(map(int,list(str(x)))))
     return True if x % sum(map(int,list(str(x)))) == 0 else False
 
+def solution2(x):
+    return x%sum([int(i) for i in str(x)]) == 0
 
-print(solution(11))
+print(solution2(11))
