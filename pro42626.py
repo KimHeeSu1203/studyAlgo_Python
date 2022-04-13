6ceaf24cafc3c609565fbd35456ec1e14cb1447c
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-04-13 10:54:17 +0900

    220413 / lv.2 / 더 맵게 /heapq 활용한 다른 사람 풀이

--- pro42626.py
+++ pro42626.py
@@ -2,6 +2,24 @@
 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
 k 이상 될때까지 반복
 """
+
+import heapq
+def solutionHeapQ(scoville, K):
+    answer = 0
+    scoville.sort()
+
+    while(scoville[0])<K:
+        if len(scoville)<=1:
+            return -1
+        else:
+            small = heapq.heappop(scoville)
+            small2 = heapq.heappop(scoville)
+            heapq.heappush(scoville, (small + (small2 * 2))) ## 최솟값이 제일 앞으로 자동으로 오게 된다
+            answer += 1
+    return answer
+
+
+
 def check(scoville, K):
     for i in scoville:
         if i < K:
@@ -28,4 +46,5 @@ def solution(scoville,K):
 
 scoville = [1, 2, 3, 9, 10, 12]
 K = 7
-print(solution(scoville,K))
+#print(solution(scoville,K))
+print(solutionHeapQ(scoville,K))
